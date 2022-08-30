b0a706ee3448edf0a09a28097e29e79aa39abb32
Sonu Kumar <sonukmrpndt@gmail.com>
2022-08-30 14:33:08 +0530

    Updates

---
+++ precache-manifest.0af350cd4a2507295d6e0a0b2434ad0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90445773910b1ac20f0a",
+    "url": "/TextUtils-App/static/css/main.97d21553.chunk.css"
+  },
+  {
+    "revision": "90445773910b1ac20f0a",
+    "url": "/TextUtils-App/static/js/main.6383233a.chunk.js"
+  },
+  {
+    "revision": "5b4025c419ee6492dff6",
+    "url": "/TextUtils-App/static/js/runtime~main.e2de129a.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/TextUtils-App/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TextUtils-App/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "116fd9d76f74d01d1551e0a636e07eae",
+    "url": "/TextUtils-App/index.html"
+  }
+];
